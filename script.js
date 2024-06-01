30c01f6c618d9029d5469443e334c9a43320a001
Siddhant Teotia <91278902+Siddhantji@users.noreply.github.com>
2024-06-01 19:39:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Person class
+// complete this js code
 function Person(name, age) {
     this.name = name;
     this.age = age;
@@ -6,9 +6,8 @@ function Person(name, age) {
 
 Person.prototype.greet = function() {
     console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
-};
+}
 
-// Employee class inherits from Person
 function Employee(name, age, jobTitle) {
     Person.call(this, name, age);
     this.jobTitle = jobTitle;
@@ -19,7 +18,7 @@ Employee.prototype.constructor = Employee;
 
 Employee.prototype.jobGreet = function() {
     console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
-};
+}
 
 // Do not change code below this line
 window.Person = Person;
